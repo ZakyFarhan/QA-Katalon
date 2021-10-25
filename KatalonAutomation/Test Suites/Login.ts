8574580558de55e3b5f7456ee23de1759a0105c3
Zaky Farhan <zakyfarhan114@gmail.com>
2021-10-25 14:39:23 +0700

    first commit katalon

---
+++ KatalonAutomation/Test Suites/Login.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Test Suite Login</description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>8f4659ec-41ca-4872-9ac0-83fafa026185</testSuiteGuid>
+   <testCaseLink>
+      <guid>73e7bd0d-8207-4c5e-8c05-3ad10f2870d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0ecd3452-7b9e-4ee6-878c-4f9cb3b557a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9edc9424-85f5-4e83-883f-ccbf56f73337</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>58f87b6e-d606-417e-98a3-10d9cf6f56fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/Login_004</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ KatalonAutomation/Test Suites/Regression Testing.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Testing</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6358a65a-5191-4808-9faa-71a9430c12c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>2a2546da-0922-401f-8b56-bfdffee33748</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Regression Testing/Regression-Testing_001</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
